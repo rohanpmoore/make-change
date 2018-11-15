599abc74448420667fcc15fb47ebea6eab3b5c65
Kristin Brewer_Lowe and Rohan Moore <rohan@>
2018-11-15 08:29:12 -0800

    Passes all tests and makes change for any amount

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,2 +1,43 @@
 def make_change(total)
+  pennies = 0
+  nickels = 0
+  dimes = 0
+  quarters = 0
+  x = total
+  change = ""
+  until x == 0
+    if x >= 25
+      quarters += 1
+      x -= 25
+    elsif x >= 10
+      dimes += 1
+      x -= 10
+    elsif x >= 5
+      nickels += 1
+      x -= 5
+    else
+      pennies += 1
+      x -= 1
+    end
+  end
+  if quarters > 1
+    change += "#{quarters} quarters "
+  elsif quarters == 1
+    change += "#{quarters} quarter "
+  end
+  if dimes > 1
+    change += "#{dimes} dimes "
+  elsif dimes == 1
+    change += "#{dimes} dime "
+  end
+  if nickels > 1
+    change += "#{nickels} nickels "
+  elsif nickels == 1
+    change += "#{nickels} nickel "
+  end
+  if pennies > 1
+    change += "#{pennies} pennies"
+  elsif pennies == 1
+    change += "#{pennies} penny"
+  end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,4 +5,16 @@ describe('make_change') do
   it('will return 1 penny if we pass in a total of 1 cent') do
     expect(make_change(1)).to(eq('1 penny'))
   end
+  it('will return the number of pennies up to 4 cents') do
+    expect(make_change(4)).to(eq('4 pennies'))
+  end
+  it('will return a combination of pennies and nickels for cents up to 9') do
+    expect(make_change(9)).to(eq('1 nickel 4 pennies'))
+  end
+  it('will return a combination of pennies, nickels, and dimes for cents up to 24') do
+    expect(make_change(17)).to(eq('1 dime 1 nickel 2 pennies'))
+  end
+  it('will return a combination of coins for any value in change') do
+    expect(make_change(94)).to(eq('3 quarters 1 dime 1 nickel 4 pennies'))
+  end
 end
